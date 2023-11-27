c4affb9243d5fe1622da41f70cf2bcd9ac976db8
sumitrapaul <sumitra.cse.just2020@gmail.com>
2023-11-28 01:42:36 +0600

    token generate in localstorage

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app=express()
 const cors = require('cors');
 const jwt =require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
 const port =process.env.PORT || 5000
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin:[
+      'http://localhost:5173'
+    ],
+    credentials:true
+  }))
 app.use(express.json())
 
+
+require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldrxrdq.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,8 +33,28 @@ async function run() {
     await client.connect();
 
     const userCollection = client.db("meal-system").collection('users');
+
+    //auth related
+    app.post('/jwt', async(req, res) =>{
+        const user = req.body
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+        // console.log(token)
+        res.send({ token })
+    })
+
+    const verifyToken = (req, res, next)=>{
+        console.log(req.headers)
+        if(!req.headers.authorization){
+            return res.status(401).send({message: 'forbidden'})
+        }
+
+        const token = req.headers.authorization.split(' ')[1]
+    
+        // next()
+    }
     //users related
-    app.get('/users', async(req, res) =>{
+    app.get('/users', verifyToken, async(req, res) =>{
+        console.log(req.headers)
         const result=await userCollection.find().toArray()
         res.send(result)
     })
